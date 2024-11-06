b6463ef08db928c754cbe9b9ef3843c3a342d086
Yong Lee <77824603+YongLeeCode@users.noreply.github.com>
2024-11-06 14:41:34 +0900

    [Feat] GNB-component (#50)
    * refactor: eslint 문제
    
    * feat: json-server 데이터 수정
    
    * Refactor: PR 리뷰 반영
    
    * feat: zustand 설치
    
    * feat: 서버 변경 및 로그아웃 기능 생성
    
    * feat: zustand 폴더 생성
    
    * feat: 로그아웃 버튼 활성화 및 로그인 상태 zustand로 저장
    
    * feat: 토글 로그아웃 기능 활성화
    
    * feat: env 파일 생성
    
    * refactor: GNB conflict 수정
    
    * feat: env 파일 gitignore에 추가
    
    * feat: 반복 리프레쉬  수정
    
    * feat: 로그인 서버 연결
    
    * feat: 회원가입 서버 연결
    
    * feat: .env 파일 깃헙으로 푸쉬에서 제외
    
    * feat: Toastify 생성
    
    - 경고 혹은 성공 팝업 창을 이한 Toastify 생성
    - 403 로그아웃 버그 해결, 그러나 400 에러가 여전히 발생함으로 주석 처리 중
    - accessToken 변수로 가지고 오지 않고 바로 적용
    
    * refactor: accessToken 파라미터 제거
    
    * feat: Toastify 생성
    
    - 경고 혹은 성공 팝업 창을 위한 Toastify 생성
    - 이름 중복 확인 서버 연결
    
    * feat: Toastify 생성
    
    * feat: 조건부 엑세스 토큰 담는 인터셉터 생성
    
    * refactor: 조건부 인터셉터를 사용으로 인해 코드 생략
    
    * feat: 3자 이상 일 때 중복 체크 시작
    
    * refactor: carousel 호버 삭제
    
    * feat: content-security-policy 메타 생성
    
    * refactor: 메타 추가로 인한 에러로 메타 삭제
    
    * refactor: 에러 메세지 변경
    
    * refactor: 코드 컨밴션  수정
    
    * refactor: 로그인 UI 수정
    
    - GNB와 로그인 화면 겹치는 현상 해결
    - 반응형 UI css 사용으로 코드 수정
    
    * refactor: 회원가입 페이지 UI 수정
    
    - GNB와 회원가입 페이지 겹친는 현상 수정
    - 반응형 UI CSS 사용으로 코드 수정
    
    * refactor: 에러로 인한 메타 삭제
    
    * feat: 로그아웃 시, 브라우저 유저 정보 삭제

--- src/apis/userApi.tsx
+++ src/apis/userApi.tsx
@@ -31,6 +31,7 @@ export const logout = () => {
     //   }
     // });
     localStorage.removeItem('accessToken');
+    localStorage.removeItem('user-storage');
     document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     window.location.reload();
     Toast('success', '로그아웃 되었습니다.');
