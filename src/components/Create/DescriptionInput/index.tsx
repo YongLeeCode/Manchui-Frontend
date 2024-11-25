6047c8d3b72da25b2238323a80f1317473f1d227
inji(FE) <155611776+inji0212@users.noreply.github.com>
2024-11-25 11:49:46 +0900

    [refactor] 생성페이지 후 경로 이동 (#103)

--- src/components/Create/DescriptionInput/index.tsx
+++ src/components/Create/DescriptionInput/index.tsx
@@ -8,20 +8,19 @@ export function DescriptionInput({ description, setDescription, error }: Descrip
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const textAreaValue = e.target.value;
     setDescription(textAreaValue);
-   
   };
 
-  const errorMessage = error==='모임 설명' ? `${error}을 입력하세요.` : error;
- return (
+  const errorMessage = error === '모임 설명' ? `${error}을 입력하세요.` : error;
+  return (
     <div>
       <h2 className="mb-3 text-base font-semibold text-gray-900"> 모임 설명 </h2>
       <textarea
         value={description || ''}
         placeholder="모임에 대한 설명을 작성해주세요."
-        className="min-h-40 w-full rounded-xl border border-blue-100 bg-blue-50 pl-2 pt-3 text-sm font-medium"
+        className="min-h-40 w-full resize-none rounded-xl border border-blue-100 bg-blue-50 pl-2 pt-3 text-sm font-medium"
         onChange={handleChange}
       />
-        {error && <p className="mt-1 text-red-500 text-sm font-medium -mb-5 ">{errorMessage}</p>}
+      {error && <p className="-mb-5 mt-1 text-sm font-medium text-red-500">{errorMessage}</p>}
     </div>
   );
 }
