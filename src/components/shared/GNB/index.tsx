d739579f7dc871741de3fc8640ee1723adc29f3b
Yong Lee <77824603+YongLeeCode@users.noreply.github.com>
2024-11-25 09:15:16 +0900

    Update index.tsx

--- src/components/shared/GNB/index.tsx
+++ src/components/shared/GNB/index.tsx
@@ -46,9 +46,6 @@ export default function GNB() {
     }
   }, [login, data, logoutStore, queryClient, updateUser]);
 
-  if (isPending) return <div>Loading...</div>;
-  if (isError) return <div>Error: {error.message}</div>;
-
   return (
     <nav className="fixed top-0 z-[9999] w-full bg-white">
       <div className="mx-auto flex h-[60px] max-w-[1500px] items-center justify-between px-4 tablet:px-6 pc:px-10">
